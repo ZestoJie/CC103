3ba100890167cb42fb555962304e7a145619444c
ZestoJie <rzesto0@gmail.com>
2026-05-12 15:03:54 +0800

    PRESENTATION 100%

--- src/main/java/com/cc103sys/cc103/Launcher.java
+++ src/main/java/com/cc103sys/cc103/Launcher.java
@@ -2,7 +2,6 @@ package com.cc103sys.cc103;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.setProperty("prism.order", "sw");
         App.main(args);
     }
 }
